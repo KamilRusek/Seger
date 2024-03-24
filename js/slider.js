b5ebc8d1ac40e2d10feb06604e5b3b3ea7266da0
KamilRusek <k.rusek.1998@gmail.com>
2024-03-24 12:31:27 +0100

    Add slider

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+let slideIndex = 0
+showSlides()
+
+function plusSlides(n) {
+	slideIndex += n
+	showSlides()
+}
+
+function showSlides() {
+	let i
+	const slides = document.getElementsByClassName('slide')
+	if (slideIndex >= slides.length) {
+		slideIndex = 0
+	}
+	if (slideIndex < 0) {
+		slideIndex = slides.length - 1
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].classList.remove('active')
+	}
+	slides[slideIndex].classList.add('active')
+	slideIndex++
+}
+
+setInterval(showSlides, 5000) // Automatyczne przewijanie co 5 sekund
