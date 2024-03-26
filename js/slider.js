0fc4575f2ba203fdd106c5e1178375024474418f
KamilRusek <k.rusek.1998@gmail.com>
2024-03-26 22:28:38 +0100

    Adding dots in the slider

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,55 @@
-let slideIndex = 0
-showSlides()
+document.addEventListener('DOMContentLoaded', function () {
+	let slideIndex = 0
 
-function plusSlides(n) {
-	showSlides((slideIndex += n))
-}
+	const slides = document.querySelectorAll('.slide')
+	const dotsContainer = document.querySelector('.dots')
 
-function showSlides() {
-	let i
-	const slides = document.getElementsByClassName('slide')
-	if (slideIndex >= slides.length) {
-		slideIndex = 0
-	}
-	if (slideIndex < 0) {
-		slideIndex = slides.length - 1
-	}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].classList.remove('active')
-	}
-	slides[slideIndex].classList.add('active')
-}
+	slides.forEach((slide, index) => {
+		const dot = document.createElement('span')
+		dot.classList.add('dot')
+		dot.addEventListener('click', () => {
+			slideIndex = index
+			showSlides()
+		})
+		dotsContainer.appendChild(dot)
+	})
 
-setInterval(function () {
-	plusSlides(1)
-}, 5000) // Automatyczne przewijanie co 5 sekund
+	function plusSlides(n) {
+		slideIndex += n
+		showSlides()
+	}
+
+	function showSlides() {
+		const slides = document.getElementsByClassName('slide')
+		const dots = document.querySelectorAll('.dot')
+		if (slideIndex >= slides.length) {
+			slideIndex = 0
+		}
+		if (slideIndex < 0) {
+			slideIndex = slides.length - 1
+		}
+		for (let i = 0; i < slides.length; i++) {
+			slides[i].classList.remove('active')
+			dots[i].classList.remove('active')
+		}
+		slides[slideIndex].classList.add('active')
+		dots[slideIndex].classList.add('active')
+	}
+
+	setInterval(function () {
+		plusSlides(1)
+	}, 5000) // Automatyczne przewijanie co 5 sekund
+
+	const prevButton = document.querySelector('.prev')
+	const nextButton = document.querySelector('.next')
+
+	prevButton.addEventListener('click', () => {
+		plusSlides(-1)
+	})
+
+	nextButton.addEventListener('click', () => {
+		plusSlides(1)
+	})
+
+	showSlides() // Pokaż pierwszy slajd po załadowaniu strony
+})
