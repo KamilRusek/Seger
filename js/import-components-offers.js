02d7c48fbeb6ec27abb1796b2d14c368f4590c4e
KamilRusek <k.rusek.1998@gmail.com>
2024-05-17 15:19:06 +0200

    Improving components

--- js/import-components-offers.js
+++ js/import-components-offers.js
@@ -1,13 +1,3 @@
-const nav = document.querySelector('.nav')
-fetch('/components/navigation-offers.html')
-	.then(res => res.text())
-	.then(data => {
-		nav.innerHTML = data
-		const parser = new DOMParser()
-		const doc = parser.parseFromString(data, 'text/html')
-		eval(doc.querySelector('script').textContent)
-	})
-
 const footer = document.querySelector('.footer')
 fetch('/components/footer-offers.html')
 	.then(res => res.text())
@@ -15,7 +5,10 @@ fetch('/components/footer-offers.html')
 		footer.innerHTML = data
 		const parser = new DOMParser()
 		const doc = parser.parseFromString(data, 'text/html')
-		eval(doc.querySelector('script').textContent)
+		const scriptElement = doc.querySelector('script') // Fix here
+		if (scriptElement) {
+			eval(scriptElement.textContent)
+		}
 	})
 
 const offers = document.querySelector('.offers')
@@ -25,5 +18,8 @@ fetch('/components/offers-offers.html')
 		offers.innerHTML = data
 		const parser = new DOMParser()
 		const doc = parser.parseFromString(data, 'text/html')
-		eval(doc.querySelector('script').textContent)
+		const scriptElement = doc.querySelector('script') // Fix here
+		if (scriptElement) {
+			eval(scriptElement.textContent)
+		}
 	})
